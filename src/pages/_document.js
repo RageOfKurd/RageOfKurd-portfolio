baaa4b1650bc995cb2d5553f7434fc2b0fe0761a
hevar <rageofkurd98@gmail.com>
2023-03-16 18:36:08 +0300

    done

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,48 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head >
+        <meta name="description" content="the creation of RageOfKurd" />
+        <meta name="keywords
+        " content="RageOfKurd" />
+        <meta name="author" content="RageOfKurd" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta httpEquiv="Content-Style-Type" content="text/css" />
+        <meta httpEquiv="Content-Script-Type" content="text/javascript" />
+        <meta name="format-detection" content="telephone=no" />
+        <meta name="format-detection" content="address=no" />
+        <meta name="format-detection" content="email=no" />
+        <meta name="robots" content="index,follow" />
+        <meta name="googlebot" content="index,follow" />
+        <meta name="google" content="notranslate" />
+        <meta name="google-site-verification" content="google-site-verification=xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx" />
+        <meta property="og:site_name" content="RageOfKurd" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="RageOfKurd" />
+        <meta property="og:description" content="the creation of RageOfKurd" />
+        <meta property="og:url" content="https://rageofkurd.com" />
+        <meta property="og:image" content="https://rageofkurd.com/images/ogp.png" />
+        <meta property="og:locale" content="ja_JP" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@RageOfKurd" />
+        <meta name="twitter:creator" content="@RageOfKurd" />
+        <meta name="twitter:title" content="RageOfKurd" />
+        <meta name="twitter:description" content="the creation of RageOfKurd" />
+        <meta name="twitter:image" content="https://rageofkurd.com/images/ogp.png" />
+        <link rel="canonical" href="https://rageofkurd.com" />
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" href="/images/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" href="/images/favicon-32x32.png" sizes="32x32" />
+        <link rel="icon" type="image/png" href="/images/favicon-16x16.png" sizes="16x16" />
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="mask-icon" href="/images/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+      </Head>
+      
       <body>
         <Main />
         <NextScript />
